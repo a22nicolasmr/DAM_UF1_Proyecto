ebc4d0645f8958dc2a4284a10bff399b68be4ffa
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2023-11-26 20:25:18 +0100

    implementadas funcionalidades pantalla principal

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainActivity.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.uf1_proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnIngredientes = findViewById<Button>(R.id.botonIngredientes)
+        val btnCategorias = findViewById<Button>(R.id.botonCategorias)
+        val btnSalir= findViewById<Button>(R.id.botonSalir)
+
+        btnIngredientes.setOnClickListener {
+            val intent = Intent(this, ButonIngredientes::class.java)
+            startActivity(intent)
+        }
+
+        btnCategorias.setOnClickListener {
+            val intent = Intent(this, ButtonCategory::class.java)
+            startActivity(intent)
+        }
+
+        btnSalir.setOnClickListener {
+            finish()
+        }
     }
 }
